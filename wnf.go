58bf99ad8286b3987a736ccf524d4d3bcc0d67ad
Kirk Scheibelhut <kjs@google.com>
2018-09-20 13:59:28 -0700

    Add in the the ability to adjust to a total distance to deal with bad math

--- wnf.go
+++ wnf.go
@@ -28,32 +28,32 @@ func PowerClimb(p, d, h, rho, vw, gr float64) float64 {
 	return Power360(p, d, h, rho, CdaClimb, vw, gr, Mt)
 }
 
-func PowerClimbLL(p float64, lls []geo.LatLng, h, rho, vw, gr float64) float64 {
-	return Power360LL(p, lls, h, rho, CdaClimb, vw, gr, Mt)
+func PowerClimbLL(p float64, lls []geo.LatLng, d, h, rho, vw, gr float64) float64 {
+	return Power360LL(p, lls, d, h, rho, CdaClimb, vw, gr, Mt)
 }
 
 func TimeClimb(t, d, h, rho, vw, gr float64) float64 {
 	return Time360(t, d, h, rho, CdaClimb, vw, gr, Mt)
 }
 
-func TimeClimbLL(t float64, lls []geo.LatLng, h, rho, vw, gr float64) float64 {
-	return Time360LL(t, lls, h, rho, CdaClimb, vw, gr, Mt)
+func TimeClimbLL(t float64, lls []geo.LatLng, d, h, rho, vw, gr float64) float64 {
+	return Time360LL(t, lls, d, h, rho, CdaClimb, vw, gr, Mt)
 }
 
 func PowerTT(p, d, h, rho, vw, gr float64) float64 {
 	return Power360(p, d, h, rho, CdaTT, vw, gr, Mt)
 }
 
-func PowerTTLL(p float64, lls []geo.LatLng, h, rho, vw, gr float64) float64 {
-	return Power360LL(p, lls, h, rho, CdaTT, vw, gr, Mt)
+func PowerTTLL(p float64, lls []geo.LatLng, d, h, rho, vw, gr float64) float64 {
+	return Power360LL(p, lls, d, h, rho, CdaTT, vw, gr, Mt)
 }
 
 func TimeTT(t, d, h, rho, vw, gr float64) float64 {
 	return Time360(t, d, h, rho, CdaTT, vw, gr, Mt)
 }
 
-func TimeTTLL(t float64, lls []geo.LatLng, h, rho, vw, gr float64) float64 {
-	return Time360LL(t, lls, h, rho, CdaTT, vw, gr, Mt)
+func TimeTTLL(t float64, lls []geo.LatLng, d, h, rho, vw, gr float64) float64 {
+	return Time360LL(t, lls, d, h, rho, CdaTT, vw, gr, Mt)
 }
 
 func Power360(p, d, h, rho, cda, vw, gr, mt float64) float64 {
@@ -64,10 +64,10 @@ func Power360(p, d, h, rho, cda, vw, gr, mt float64) float64 {
 	return s / 360
 }
 
-func Power360LL(p float64, lls []geo.LatLng, h, rho, cda, vw, gr, mt float64) float64 {
+func Power360LL(p float64, lls []geo.LatLng, d, h, rho, cda, vw, gr, mt float64) float64 {
 	s := 0.0
 	for dw := 0; dw < 360; dw++ {
-		s += PowerLL(p, lls, h, rho, cda, vw, float64(dw), gr, mt)
+		s += PowerLL(p, lls, d, h, rho, cda, vw, float64(dw), gr, mt)
 	}
 	return s / 360
 }
@@ -80,10 +80,10 @@ func Time360(t, d, h, rho, cda, vw, gr, mt float64) float64 {
 	return s / 360
 }
 
-func Time360LL(t float64, lls []geo.LatLng, h, rho, cda, vw, gr, mt float64) float64 {
+func Time360LL(t float64, lls []geo.LatLng, d, h, rho, cda, vw, gr, mt float64) float64 {
 	s := 0.0
 	for dw := 0; dw < 360; dw++ {
-		s += TimeLL(t, lls, h, rho, cda, vw, float64(dw), gr, mt)
+		s += TimeLL(t, lls, d, h, rho, cda, vw, float64(dw), gr, mt)
 	}
 	return s / 360
 }
@@ -94,9 +94,9 @@ func Power(p, d, h, rho, cda, vw, dw, db, gr, mt float64) float64 {
 	return p2 / p
 }
 
-func PowerLL(p float64, lls []geo.LatLng, h, rho, cda, vw, dw, gr, mt float64) float64 {
-	t := timeLL(p, lls, calc.Rho(h, calc.G), cda, 0, dw, gr, mt)
-	p2 := powerLL(t, lls, rho, cda, vw, dw, gr, mt)
+func PowerLL(p float64, lls []geo.LatLng, d, h, rho, cda, vw, dw, gr, mt float64) float64 {
+	t := timeLL(p, lls, d, calc.Rho(h, calc.G), cda, 0, dw, gr, mt)
+	p2 := powerLL(t, lls, d, rho, cda, vw, dw, gr, mt)
 	return p2 / p
 }
 
@@ -106,9 +106,9 @@ func Time(t, d, h, rho, cda, vw, dw, db, gr, mt float64) float64 {
 	return p2 / p1
 }
 
-func TimeLL(t float64, lls []geo.LatLng, h, rho, cda, vw, dw, gr, mt float64) float64 {
-	p1 := powerLL(t, lls, calc.Rho(h, calc.G), cda, 0, dw, gr, mt)
-	p2 := powerLL(t, lls, rho, cda, vw, dw, gr, mt)
+func TimeLL(t float64, lls []geo.LatLng, d, h, rho, cda, vw, dw, gr, mt float64) float64 {
+	p1 := powerLL(t, lls, d, calc.Rho(h, calc.G), cda, 0, dw, gr, mt)
+	p2 := powerLL(t, lls, d, rho, cda, vw, dw, gr, mt)
 	return p2 / p1
 }
 
@@ -118,9 +118,9 @@ func Power2(p, d, rho1, rho2, cda, vw1, vw2, dw1, dw2, db, gr, mt float64) float
 	return p2 / p
 }
 
-func Power2LL(p float64, lls []geo.LatLng, rho1, rho2, cda, vw1, vw2, dw1, dw2, gr, mt float64) float64 {
-	t := timeLL(p, lls, rho1, cda, vw1, dw1, gr, mt)
-	p2 := powerLL(t, lls, rho2, cda, vw2, dw2, gr, mt)
+func Power2LL(p float64, lls []geo.LatLng, d, rho1, rho2, cda, vw1, vw2, dw1, dw2, gr, mt float64) float64 {
+	t := timeLL(p, lls, d, rho1, cda, vw1, dw1, gr, mt)
+	p2 := powerLL(t, lls, d, rho2, cda, vw2, dw2, gr, mt)
 	return p2 / p
 }
 
@@ -130,9 +130,9 @@ func Time2(t, d, rho1, rho2, cda, vw1, vw2, dw1, dw2, db, gr, mt float64) float6
 	return p2 / p1
 }
 
-func Time2LL(t float64, lls []geo.LatLng, rho1, rho2, cda, vw1, vw2, dw1, dw2, gr, mt float64) float64 {
-	p1 := powerLL(t, lls, rho1, cda, vw1, dw1, gr, mt)
-	p2 := powerLL(t, lls, rho2, cda, vw2, dw2, gr, mt)
+func Time2LL(t float64, lls []geo.LatLng, d, rho1, rho2, cda, vw1, vw2, dw1, dw2, gr, mt float64) float64 {
+	p1 := powerLL(t, lls, d, rho1, cda, vw1, dw1, gr, mt)
+	p2 := powerLL(t, lls, d, rho2, cda, vw2, dw2, gr, mt)
 	return p2 / p1
 }
 
@@ -140,53 +140,61 @@ func time(p, d, rho, cda, vw, dw, db, gr, mt float64) float64 {
 	return calc.Time(p, d, rho, cda, calc.Crr, vw, dw, db, gr, mt, calc.G, calc.Ec, calc.Fw)
 }
 
-func timeLL(p float64, lls []geo.LatLng, rho, cda, vw, dw, gr, mt float64) float64 {
+func timeLL(p float64, lls []geo.LatLng, d, rho, cda, vw, dw, gr, mt float64) float64 {
 	t := 0.0
 	if len(lls) <= 1 {
 		return t
 	}
 
+	// We need to determine the computed distance D and figure out how it compares
+	// to the provided distance d so that we can adjust each individual computed
+	// distance segment proportionally.
+	td := totalDistance(lls, gr)
+
 	ll := lls[0]
 	for i := 1; i < len(lls); i++ {
-		t += time(p, distance(ll, lls[i], gr), rho, cda, vw, dw, geo.Bearing(ll, lls[i]), gr, mt)
+		t += time(p, distance(ll, lls[i], gr)*d/td, rho, cda, vw, dw, geo.Bearing(ll, lls[i]), gr, mt)
+		ll = lls[i]
 	}
 
 	return t
 }
-
-// TODO(kjs): confirm sum of segment distances (with hypotenuse) = strava reported distance!
 
 func power(t, d, rho, cda, vw, dw, db, gr, mt float64) float64 {
 	vg := d / t
 	return calc.Psimp(rho, cda, calc.Crr, calc.Va(vg, vw, dw, db), vg, gr, mt, calc.G, calc.Ec, calc.Fw)
 }
 
-func powerLL(tt float64, lls []geo.LatLng, rho, cda, vw, dw, gr, mt float64) float64 {
+func powerLL(tt float64, lls []geo.LatLng, d, rho, cda, vw, dw, gr, mt float64) float64 {
 	p := 0.0
 	if len(lls) <= 1 {
 		return p
 	}
 
 	td := 0.0
-	var ds []float64
+	var dis []float64
 	var dbs []float64
 
 	ll := lls[0]
 	for i := 1; i < len(lls); i++ {
-		d := distance(ll, lls[i], gr)
-		td += d
-		ds = append(ds, d)
+		di := distance(ll, lls[i], gr)
+		td += di
+		dis = append(dis, di)
 		dbs = append(dbs, geo.Bearing(ll, lls[i]))
+		ll = lls[i]
 	}
 
-	for i := 0; i < len(ds); i++ {
-		d := ds[i]
-		// The time t to complete this small segment compared to the total time tt is
-		// proportiona to the segment distance d compared to the total distance td.
-		t := tt * (d / td)
-		// We need to weight this segments contribution to the total average power by
-		// the fraction of the total time that was spent at this power.
-		p += power(t, d, rho, cda, vw, dw, dbs[i], gr, mt) * (t / tt)
+	for i := 0; i < len(dis); i++ {
+		// Adjust the computed segment distance di to be proportional to the
+		// total provided distance d.
+		dadj := dis[i] * (d / td)
+		// The time t to complete this small segment compared to the total time tt
+		// is proportional to the adjusted segment distance dadj compared to the
+		// total provided distance d.
+		t := tt * (dadj / d)
+		// We need to weight this segments contribution to the total average power
+		// by the fraction of the total time that was spent at this power.
+		p += power(t, dadj, rho, cda, vw, dw, dbs[i], gr, mt) * (t / tt)
 	}
 
 	return p
@@ -197,6 +205,20 @@ func distance(p1, p2 geo.LatLng, gr float64) float64 {
 	// NOTE: Assuming even average gradient for the entire track.
 	rise := gr * run
 	return math.Sqrt(run*run + rise*rise)
+}
+
+func totalDistance(lls []geo.LatLng, gr float64) float64 {
+	d := 0.0
+	if len(lls) <= 1 {
+		return d
+	}
+
+	ll := lls[0]
+	for i := 1; i < len(lls); i++ {
+		d += distance(ll, lls[i], gr)
+		ll = lls[i]
+	}
+	return d
 }
 
 type course struct {
